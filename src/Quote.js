ba6bc9b5f128767ed36f231ff14b082fa0613cf7
Param Keshkamat <71186376+paramkeshkamat@users.noreply.github.com>
2021-01-05 21:33:25 +0530

    Update Quote.js

--- src/Quote.js
+++ src/Quote.js
@@ -8,13 +8,13 @@ const url = "https://type.fit/api/quotes";
 const Quote = () => {
   const [quotes, setQuotes] = useState([]);
   const [randomQuote, setRandomQuote] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getQuotes = async () => {
     const response = await axios.get(url);
     setQuotes(response.data);
     setRandomQuote(response.data[0]);
-    setLoading(false);
+    setIsLoading(false);
   };
 
   const getRandomQuote = () => {
@@ -31,7 +31,7 @@ const Quote = () => {
     getQuotes();
   }, []);
 
-  if (loading) {
+  if (isLoading) {
     return <Loading />;
   }
 
